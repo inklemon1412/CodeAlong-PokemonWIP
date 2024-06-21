92e90cfc1795ce8abedb3139fbc435d562a43576
inklemon1412 <jonasnie@getacademy.no>
2024-06-21 15:14:02 +0200

    Add project files.

---
+++ CodeAlong Pokemon/Pokemon.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CodeAlong_Pokemon
+{
+    internal class Pokemon
+    {
+        public string _name;
+        public string _type;
+        public int _strength;
+        public int _health;
+
+        public Pokemon(string Name, string Type, int Strength, int Health)
+        {
+            _name = Name;
+            _type = Type;
+            _strength = Strength;
+            _health = Health;
+        }
+        public Pokemon()
+        {
+
+        }
+
+        public void PokemonMenu(List<Pokemon> allPokemon)
+        {
+            PrintAllPokemon(allPokemon);
+        }
+
+        public void PrintAllPokemon(List<Pokemon> allPokemon)
+        {
+
+            foreach (var pokemon in allPokemon)
+            {
+                Console.WriteLine($"Pokemon Name: {pokemon._name}");
+                Console.WriteLine($"Pokemon Type: {pokemon._type}");
+                Console.WriteLine($"Pokemon Health: {pokemon._health}");
+                Console.WriteLine($"Pokemon Strength: {pokemon._strength}");
+            }
+        }
+        public void choosePokemon(List<Pokemon> allPokemon)
+        {
+            while (true)
+            {
+
+                Console.WriteLine("Which Pokemon would you like to choose?");
+                var ChosenPokemon = Console.ReadLine();
+                switch (ChosenPokemon)
+                {
+                    case "1":
+                        ChooseEevee();
+                        break;
+                    case "Pikachu":
+                        ChoosePikachu();
+                        break;
+                    case "Grovyle":
+                        ChooseGrovyle();
+                        break;
+
+                }
+            }
+
+        }
+        public void ChooseEevee()
+        {
+
+        }
+        public void ChoosePikachu()
+        {
+
+        }
+        public void ChooseGrovyle()
+        {
+
+        }
+        public void FightMenu(List<Pokemon> allPokemon)
+        {
+            while (chosenPokemon._health  > 0 && enemyPokemon._health > 0) 
+            {
+
+            }
+        }
+    }
+}
+
+           
+ 
+
+    
+
---
+++ CodeAlong Pokemon/Program.cs
@@ -0,0 +1,53 @@
+﻿/*
+ Du skal lage et utvalg av forskjellige pokemon med ulik styrke og helse som lever i pokemonworld. 
+Brukeren skal kunne velge en av disse han ønsker å spille med.
+Motstanderen velges så tilfeldig blant utvalget som er igjen.
+Disse to skal kjempe mot hverandre til en av dem har 0 i helse. 
+Consollen logger ut hvor mye helse de har igjen for hvert slag. Brukeren skal også kunne velge å “fange en ny pokemon” som blir med i utvalget hans.
+ */
+using CodeAlong_Pokemon;
+
+Pokemon pokemon = new Pokemon();
+List<Pokemon> allPokemon = new List<Pokemon>()
+            {
+                new Pokemon("Pikachu", "Electric", 40, 50),
+                new Pokemon("Eevee", "Normal", 20, 10),
+                new Pokemon("Grovyle", "Grass", 60, 100),
+            };
+Main();
+
+void Main()
+{
+    while (true)
+    {
+        Console.Clear();
+
+        Console.WriteLine("What would you like to do?");
+
+        MainMenu();
+    }
+}
+void MainMenu()
+{
+    Console.WriteLine("1) See Pokemon");
+    Console.WriteLine("2) Choose a pokemon to fight with");
+
+    var userInput = Console.ReadLine();
+    switch (userInput)
+    {
+        case "1":
+        pokemon.PokemonMenu(allPokemon); 
+           break;
+        case "2":
+        pokemon.choosePokemon(allPokemon);
+           break;
+        case "3":
+       pokemon.FightMenu(allPokemon);
+    break;
+            
+        
+        
+        
+        
+    }
+}
